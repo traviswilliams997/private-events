dcefe8a7ba0967b6ba2e73a53aaa88d92957bb3a
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-05-14 20:51:46 -0500

    Change user id to creator id

---
+++ db/migrate/20230515015015_change_foreign_key_name_for_events.rb
@@ -0,0 +1,6 @@
+class ChangeForeignKeyNameForEvents < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :events, :user_id, :creator_id
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_15_005500) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_15_015015) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.datetime "date"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_15_005500) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users", column: "creator_id"
 end
