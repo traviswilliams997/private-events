62017d938ac9fca18849ee8381be3a9ff0db6180
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-05-15 05:55:51 -0500

    Add event functionality

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,41 @@
 class EventsController < ApplicationController
+
+  before_action :set_event, only: %i[show edit]
+
   def index
+    @events = Event.all
   end
+  def show 
+  end
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+def create 
+
+  @event = current_user.created_events.build(event_params)
+  @event.creator_id = current_user.id
+
+  if @event.save
+    flash[:success] = "Event Successfully Created"
+
+    redirect_to root_path
+  else
+    flash[alert] = @event.errors.count
+    render :new, status: :unprocessable_entity, alert: 'Event not created'
+  end
+end 
+
+  def edit 
+  end
+
+  private 
+  def set_event
+    @event = Event.find(params[:id])
+  end
+  def event_params
+    params.require(:event).permit(:title, :date, :location)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
    root "events#index"
+
+   resources :events
+   resources :users, only: [:show]
 
   # Defines the root path route ("/")
   # root "articles#index"
